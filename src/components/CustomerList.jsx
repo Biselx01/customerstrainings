0b86167bb96731837801ec955c431ef8f5fe90d7
Biselx01 <biselxthomas@gmail.com>
2024-11-12 19:44:18 +0200

    CSV export without buttons

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,5 +1,8 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback, useRef } from "react";
 import { AgGridReact } from "ag-grid-react";
+import { ClientSideRowModelModule } from "@ag-grid-community/client-side-row-model";
+import { ModuleRegistry } from "@ag-grid-community/core";
+import { CsvExportModule } from "@ag-grid-community/csv-export";
 import { fetchCustomers, deleteCustomer } from "../services/api";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
@@ -8,7 +11,10 @@ import EditCustomer from "./EditCustomer";
 import Button from "@mui/material/Button";
 import Snackbar from '@mui/material/Snackbar';
 
+ModuleRegistry.registerModules([ClientSideRowModelModule, CsvExportModule]);
+
 function CustomerList() {
+    const gridRef = useRef();
     const [customer, setCustomer] = useState([]);
     const [open, setOpen] = useState(false);
     const [colDefs] = useState([
@@ -20,10 +26,11 @@ function CustomerList() {
         { field: "email", filter: true},
         { field: "phone", filter: true },
         {
-            cellRenderer: params => <EditCustomer handleFetch={handleFetch} data={params.data} />,
-            width: 120
+            cellRenderer: params => <EditCustomer 
+                handleFetch={handleFetch} 
+                data={params.data} />,
+                width: 120
         },
-        
         {
             cellRenderer: params => <Button 
                 color="error" 
@@ -56,15 +63,22 @@ function CustomerList() {
         }
     }
 
+    const onBtnExport = useCallback((params) => {
+        params.columnKeys = ["firstname", "lastname", "streetaddress", "postcode", "city", "email", "phone"];
+        gridRef.current.api.exportDataAsCsv(params);
+    }, []);
+
     return (
         <>
         <AddCustomer handleFetch={handleFetch}/>
+          <Button variant="outlined" onClick={onBtnExport}>Export to CSV</Button>
         <div className="ag-theme-material" style={{ height: 400, width: '100%' }}>
             <AgGridReact
+                ref={gridRef}
                 columnDefs={colDefs}
                 rowData={customer}
                 pagination={true}
-                paginationPageSize={10}
+                paginationPageSize={10}  
             />
         </div>
             <Snackbar
