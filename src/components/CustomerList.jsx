e5cf7ade49e45fcdfa55b396b3d82542caab3cb0
Biselx01 <biselxthomas@gmail.com>
2024-11-12 11:15:51 +0200

    Delete Customer whit confirmation message

--- src/components/CustomerList.jsx
+++ src/components/CustomerList.jsx
@@ -1,14 +1,16 @@
 import { useState, useEffect } from "react";
 import { AgGridReact } from "ag-grid-react";
-import { fetchCustomers } from "../services/api";
+import { fetchCustomers, deleteCustomer } from "../services/api";
 import "ag-grid-community/styles/ag-grid.css";
 import "ag-grid-community/styles/ag-theme-material.css";
 import AddCustomer from "./AddCustomer";
 import EditCustomer from "./EditCustomer";
+import Button from "@mui/material/Button";
+import Snackbar from '@mui/material/Snackbar';
 
 function CustomerList() {
     const [customer, setCustomer] = useState([]);
-    //const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
     const [colDefs] = useState([
         { field: "firstname", filter: true },
         { field: "lastname", filter: true },
@@ -17,10 +19,20 @@ function CustomerList() {
         { field: "city", filter: true },
         { field: "email", filter: true},
         { field: "phone", filter: true },
-          {
+        {
             cellRenderer: params => <EditCustomer handleFetch={handleFetch} data={params.data} />,
             width: 120
         },
+        
+        {
+            cellRenderer: params => <Button 
+                color="error" 
+                size="small"
+                onClick={() => handleDelete(params.data._links.self.href)}> 
+                Delete 
+            </Button>,
+            width: 120
+        }
     ]);
 
         useEffect(() => {
@@ -31,6 +43,17 @@ function CustomerList() {
         fetchCustomers()
         .then((data) => {setCustomer(data._embedded.customers)})
         .catch((err) => console.error(err));
+    }
+
+        const handleDelete = (url) => {
+        if (window.confirm("Are you sure you want to delete?")) {
+            deleteCustomer(url)
+            .then (() => {
+                handleFetch()
+                setOpen(true)
+            })
+            .catch(err => console.error(err))
+        }
     }
 
     return (
@@ -44,6 +67,13 @@ function CustomerList() {
                 paginationPageSize={10}
             />
         </div>
+            <Snackbar
+                open={open}
+                autoHideDuration={3000}
+                onClose={() => setOpen(false)}
+                message="Customer deleted"
+                anchorOrigin={{ vertical: "top", horizontal: "center" }}
+            />
         </>
     );
 }
--- src/services/api.js
+++ src/services/api.js
@@ -37,3 +37,13 @@ export function updateCustomer(url, updatedCustomer) {
     return response.json();
   });
 }
+
+export function deleteCustomer(url) {
+  return fetch(url, {
+    method: "DELETE",
+  }).then((response) => {
+    if (!response.ok)
+      throw new Error("Error in delete : " + response.statusText);
+    return response.json();
+  });
+}
