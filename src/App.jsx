8bc39fdb53c99dbad60f51dd82aaf5f448c385e4
Biselx01 <biselxthomas@gmail.com>
2024-11-12 10:10:56 +0200

    Creation of Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
+import { useState } from 'react'
 import CustomerList from './components/CustomerList'
 import TrainingList from './components/TrainingList'
 import AppBar from '@mui/material/AppBar'
 import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
+import { Outlet } from 'react-router-dom';
 import Container from '@mui/material/Container'
 import CssBaseline from '@mui/material/CssBaseline'
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Box from '@mui/material/Box';
+
+function CustomTabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+    </div>
+  );
+}
+
+CustomTabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
 
 function App() {
+
+  const [value, setValue] = useState(0);
+
+    const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
 
   return (
     <Container maxWidth="xl"> 
@@ -17,8 +58,21 @@ function App() {
           </Typography>
         </Toolbar>
       </AppBar>
-    <CustomerList />
-    <TrainingList />
+      <Box sx={{ width: '100%' }}>
+        <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+          <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+            <Tab label="Customers" {...a11yProps(0)} />
+            <Tab label="Trainings" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+          <CustomTabPanel value={value} index={0}>
+            <CustomerList />
+          </CustomTabPanel>
+          <CustomTabPanel value={value} index={1}>
+            <TrainingList />
+          </CustomTabPanel>
+      </Box>
+      <Outlet />
     <CssBaseline />
     </Container>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,34 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import CustomerList from './components/CustomerList.jsx'
+import TrainingList from './components/TrainingList.jsx'
+import { createHashRouter, RouterProvider  } from 'react-router-dom';
+
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [                       
+      {
+        path: "Customer",
+        element: <CustomerList />,
+      },
+      {
+        path: "Training",
+        element: <TrainingList />,
+      }
+    ]
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
+
+
+
+
+
